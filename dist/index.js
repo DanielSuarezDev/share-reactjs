28f0b192db0134c4b9fc97137a771f525020f905
DanielSuarez <danielsuarezdev@gmail.com>
2022-02-15 23:36:21 -0500

    feat: agregamos semantic-version

---
+++ dist/index.js
@@ -0,0 +1,9 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.Header = exports.Page = exports.Button = void 0;
+var Button_1 = require("./stories/components/Atoms/Button");
+Object.defineProperty(exports, "Button", { enumerable: true, get: function () { return Button_1.Button; } });
+var Page_1 = require("./stories/components/Molecules/Page");
+Object.defineProperty(exports, "Page", { enumerable: true, get: function () { return Page_1.Page; } });
+var Header_1 = require("./stories/components/Organisms/Header");
+Object.defineProperty(exports, "Header", { enumerable: true, get: function () { return Header_1.Header; } });
--- src/stories/components/Organisms/Header.tsx
+++ src/stories/components/Organisms/Header.tsx
@@ -1,13 +1,13 @@
 import React from "react";
 
-import { Button } from "./components/Atoms/Button";
+import { Button } from "../Atoms/Button";
 import "./header.css";
 
 type User = {
   name: string;
 };
 
-interface HeaderProps {
+export interface HeaderProps {
   user?: User;
   onLogin: () => void;
   onLogout: () => void;
